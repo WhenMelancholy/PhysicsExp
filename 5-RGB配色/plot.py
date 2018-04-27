6bd9fa603154378e293868c46943c1dcaf437b95
ustcpetergu <guyimin@mail.ustc.edu.cn>
2018-04-27 20:36:27 +0800

    another.

--- 5-RGB配色/plot.py
+++ 5-RGB配色/plot.py
@@ -20,7 +20,7 @@ fin = open('./data.txt', 'r')
 for i in range(3):
     U1[i], I1[i] = readpart(fin, 2, need=0b100)
 for i in range(3):
-    U2[i], I2[i] = readpart(fin, 2, need=0b100)
+    I2[i], U2[i] = readpart(fin, 2, need=0b100)
 fin.close()
 
 #data process
@@ -32,7 +32,8 @@ for i in range(3):
     #计算(估算)发光波长
     slope, intercept = linear_regression(U1[i][9:], I1[i][9:], simple=1, quiet=1)
     #$\Lamba = 1240 / E_g (nm) $ E_g eV
-    print(name[i], '波长估算值(nm):', 1240/(math.fabs(intercept) / math.fabs(slope)))
+    U0 = math.fabs(intercept) / math.fabs(slope)
+    print(name[i], '电压:', U0, '波长估算值(nm):', 1240/U0)
 
 plt.xlabel('电压U / V')
 plt.ylabel('电流I / A')
